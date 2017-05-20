6f63bcba89fec1864ccd749004cecb07ef200cd8
Trevor Allen <trevoristall@gmail.com>
2017-05-20 16:07:59 -0400

    express over koa, heroku timeout prevention

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node dist/app.js
+web: npm run build && npm run start
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+
+// prevent heroku app sleep
+setInterval(() => {
+    http.get('http://t-atlas.herokuapp.com');
+}, 300000); // every 5 minutes (300000)
+
+app.get('/', (req, res) => {
+    res.send(require('../resume.json'));
+})
+
+app.listen(3000);
