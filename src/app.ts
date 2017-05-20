4e9180e9a0850f123e7d1f3a5f535d3e694f9475
Trevor Allen <trevoristall@gmail.com>
2017-05-20 16:28:56 -0400

    port maybe working now?

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ const express = require('express');
 const http = require('http');
 const app = express();
 
-// prevent heroku app sleep
+// prevent Heroku app sleep
 setInterval(() => {
     http.get('http://t-atlas.herokuapp.com');
-}, 300000); // every 5 minutes (300000)
+}, 600000); // every 10 minutes
 
 app.get('/', (req, res) => {
     res.json(require('../resume.json'));
 })
 
-app.listen(3000);
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'));
+});
